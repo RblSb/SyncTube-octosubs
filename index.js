71a6b99312ea72a6de759eb64771cae07b767359
RblSb <msrblsb@gmail.com>
2020-04-25 02:02:34 +0300

    Fix player refresh button

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ if (!window.SubtitlesOctopus) {
 }
 function init() {
   let instance;
+  JsApi.notifyOnVideoRemove(item => {
+    if (instance) {
+      instance.dispose();
+      instance = null;
+    }
+  });
   JsApi.notifyOnVideoChange(item => {
-    if (instance) instance.dispose();
+    if (instance) {
+      instance.dispose();
+      instance = null;
+    }
     if (item.duration < 60 * 5) return;
     if (item.url.indexOf('.mp4') == -1) return;
     const subsUrl = item.url.replace('.mp4', '.ass');
+    // const subsUrl = `${mainUrl}/test.ass`;
     fetchStatus(subsUrl, status => {
       if (status != 200) return;
       const options = {
         video: document.getElementById('videoplayer'),
-        subUrl: subsUrl, // `${mainUrl}/test.ass`,
+        subUrl: subsUrl,
         workerUrl: `${mainUrl}/subtitles-octopus-worker.js`,
         legacyWorkerUrl: `${mainUrl}/subtitles-octopus-worker-legacy.js`
       };
